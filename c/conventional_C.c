b7aa2e5507869eb9cdade4f2f318dbe5b6899b8b
xqch1983 <xieqingchun@126.com>
2017-12-26 15:22:31 +0800

    initialize the repository

---
+++ c/conventional_C.c
@@ -0,0 +1,18 @@
+
+#include "stdio.h"
+main()
+{
+	printf("hello world!");
+}
+
+
+//=================================
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a = 9;
+	printf("%f\n", sqrt(x));
+
+}
---
+++ c/standard_C.c
@@ -0,0 +1,16 @@
+
+#include "cstdio"
+int main()
+{
+	printf("hello world!");
+	return 0;
+}
+
+#include <cstdio>
+#include <cmath>
+int main()
+{
+	float x = 9;
+	printf("%f\n", sqrt(x));
+	return 0;
+}
