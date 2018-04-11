d0dcb7ebb9c31546eaf73f9bb8f24b52c49be941
xqch1983 <xieqingchun@126.com>
2018-04-11 16:26:56 +0800

    Add modify and delete functions in Linklist.

--- cplusplus/LinkList.cpp
+++ cplusplus/LinkList.cpp
@@ -5,7 +5,8 @@ class Node {
 public:
 	int data;
 	Node *next;
-	Node(int _data) {
+	Node(int _data)
+	{
 		data = _data;
 		next = NULL;
 	}
@@ -18,6 +19,11 @@ public:
 	}
 	void Insert(Node *node, int position);
 	void Output();
+	void Delete(int position);
+	void Delete(Node _data);
+	void Modify(int position, int _data);
+	int Size();
+
 private:
 	Node * head;
 };
@@ -31,7 +37,7 @@ void Linklist::Insert(Node *node, int position)
 	{
 		node->next = head;
 		head = node;
-		return;
+
 	}
 	Node * current_node = head;
 	int i = 0;
@@ -46,6 +52,51 @@ void Linklist::Insert(Node *node, int position)
 		current_node->next = node;
 	}
 }
+
+void Linklist::Modify(int position, int _data)
+{
+	Node * current_node = head;
+
+	if (position < 0 && position > Size())
+	{
+
+		cout << "the position is out the range of Linklist" << endl;
+		return;
+
+	}
+	if (head == NULL && position == 0)
+	{
+		cout << "The linklist is Null" << endl;
+		return;
+	}
+	for (int i = 1; i < position; i++)
+	{
+		current_node = current_node->next;
+	}
+	current_node->data = _data;
+
+	cout << "the position and data to modify is " << position << _data << endl;
+
+}
+
+
+int Linklist::Size()
+{
+	int count = 0;
+	if (head == NULL)
+		return count;
+	else
+	{
+		Node *current_head = head;
+		while (current_head != NULL)
+		{
+			current_head = current_head->next;
+			count++;
+		}
+		return count;
+	}
+}
+
 void Linklist::Output()
 {
 	if (head == NULL)
@@ -53,19 +104,86 @@ void Linklist::Output()
 	Node *current_node = head;
 	while (current_node != NULL)
 	{
-		cout << current_node->data;
+		cout << current_node->data << endl;
 		current_node = current_node->next;
+
 	}
 	cout << endl;
+}
+
+
+void Linklist::Delete(int position)
+{
+	if (head == NULL && position == 0)
+		return;
+	if (position > Size())
+	{
+		cout << "the position is big than linklist size" << endl;
+		return;
+	}
+
+	Node *current_node = head;
+	if (position == 1)
+		head = current_node->next->next;
+
+	else
+	{
+		for (int i = 0; i < position - 2; i++)
+		{
+			current_node = current_node->next;
+		}
+		cout << "deletet number is " << current_node->next->data << endl;
+		current_node->next = current_node->next->next;
+	}
+}
+
+void Linklist::Delete(Node node)
+{
+	if (head == NULL)
+		return;
+	Node *pre_current_node = head, *current_node = head;;
+	while (current_node != NULL)
+	{
+		if (current_node->data != node.data)
+
+		{
+			pre_current_node = current_node;
+			current_node = current_node->next;
+		}
+		else
+		{
+			cout << "deletet number is " << current_node->data << endl;
+			pre_current_node->next = current_node->next;
+			current_node = pre_current_node;
+
+		}
+
+	}
 }
 int main()
 {
 	Linklist linklist;
 	for (int i = 0; i <= 10; i++)
 	{
-		Node *node = new Node(i);
-		linklist.Insert(node, i - 1);
+		Node *node = new Node(i + 1);
+		linklist.Insert(node, i);
 	}
+	int size = linklist.Size();
+	cout << "<<<the size of the linklist is>>>:" << size << endl;
+	linklist.Output();
+	Node oneNode(5);
+	cout << "after delete xxxxxxxxxxxxxxxxxxxx" << endl;
+
+	//linklist.Delete(5);
+	linklist.Delete(oneNode);
+	size = linklist.Size();
+	cout << "After delete one item,the size of the linklist is:" << size << endl;
+	linklist.Output();
+
+	linklist.Modify(1, 10);
+	cout << "after Modify xxxxxxxxxxxxxxxxxxxx" << endl;
 	linklist.Output();
 	return 0;
+
+
 }
